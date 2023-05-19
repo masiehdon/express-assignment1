617f58f40a349d7222fdc59fcad91dbdab20ab4b
Masih <masihdon@hotmail.de>
2023-05-19 12:21:34 +0200

    status handling, api key validation

--- app.js
+++ app.js
@@ -1,74 +1,108 @@
 const express = require('express');
 const _ = require('lodash');
 const ejs = require('ejs');
-const path = require('path');
 const nodemon = require('nodemon');
-const movies = require('./views/movies.js');
-const methodOverride = require('method-override');
+const films = require('./routes/movies.js');
+
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(methodOverride('_method'));
+
 
 const port = 4000;
 app.use(express.json())
 
-// GET
-app.get('/', (req, res) => {
-  const searchTerm = req.query.search || '';
-  const filteredMovies = _.filter(
-    movies,
-    (movie) =>
-      _.includes(movie.Title.toLowerCase(), searchTerm.toLowerCase()) ||
-      _.includes(movie, searchTerm.toLowerCase())
-  );
-  res.render('index', { movies: filteredMovies, searchTerm: searchTerm });
-});
+let movies = films;
 
+const apiKey = "5"
 
-app.get('/movies', (req, res) => {
-res.render("movies", { Title: movies.Title})
-})
-
-//POST
-app.get('/addMovie', (req, res) => {
-  const title = req.query.title || '';
-  res.render('addMovie', { Title: title });
-});
-
-app.post('/addMovie', (req, res) => {
-  const newMovie = { Title: req.body.movie };
-  movies.unshift(newMovie);
-  res.redirect('/');
-});
-
-
-// PUT
-app.get('/updateMovie', (req, res) => {
-    res.render('updateMovie');
-});
-
-
-// DELETE
-app.get('/deleteMovie', (req, res) => {
-  const title = req.query.title
-  res.render('deleteMovie', { Title: title });
-});
-
-// app.delete('/deleteMovie', (req, res) => {
-//   const imdbID = req.params.imdbID;
-//   // _.remove(movies, { imdbID: imdbID });
-//   // const deletedMovie = _.filter(movies, (movie) => {});
-//   res.redirect('/');
-// });
+const validKey = (req, res, next) => {
+  const key = req.query.key;
+  if (!validKey) {
+    res.status(404).send('<h1>Page not found!</h1>');
+  } else if(key != apiKey) {
+    res.status(403).send('Not authorized')
+  } else {
+    next()
+  }
+}
 
 app.use((req, res, next) => {
-  res.status(404).send('Page not found!');
+  validKey(req, res, next)
+})
+
+// GET
+app.get('/', (req, res) => {
+  res.send('Page successfully loaded')
+})
+
+// GET Movie by id 
+app.get('/movies/id/:id', (req, res) => {
+  const id = req.params.id;
+  const movie = movies.find((movie) => movie.imdbID === id)
+  movie ?  res.send(movie.Title) : res.status(404).send('No movie found!')
+ 
+})
+
+// GET movie by title
+app.get('/movies/title/:title', (req, res) => {
+  const title = req.params.title;
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.Title.toLowerCase().replace(/\s/g, '').includes(title.toLowerCase().replace(/\s/g, '')
+      ));
+  const movieTitle = filteredMovies.map(movie => movie.Title)
+  res.send(movieTitle);
+})
+
+// DELETE movie by ID
+app.delete('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  const deletedMovie = movies.find((movie) => movie.imdbID === id); 
+  newMovieList = movies.filter((movie) => movie.imdbID !== id);
+  movies = newMovieList
+  res.send(`'${deletedMovie.Title}' was removed`)
+  })
+
+  // POST - Add a new movie
+  let newID = 1
+app.post('/movies', (req, res) => {
+  const newMovie = req.body;
+  movies.push(newMovie)
+  
+  res.json(newMovie)
 });
 
+
+// PUT - Update a movie
+const updatedMovie = {
+  "imdbID": newID++,
+  "Title": 'New updated Movie',
+  "Year": 2023,
+};
+  
+  
+app.put('/movies/:id', (req, res) => {
+  const id = req.params.id
+  let update = movies.find(movie => movie.imdbID === id)
+  if (!update) {
+    res.send('No movie found')
+  } else {
+    update = updatedMovie
+    res.send(updatedMovie)
+  }
+})
+
+// Handling 404 status
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found!</h1>')
+  next()
+})
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log('running on port 4000');
+
+
+
+
+})
